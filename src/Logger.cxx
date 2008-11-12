231e4d8effbea805c5e12ad03eca42d80af18a5a
r0q <deen@l.local>
2008-11-12 23:47:07 +0100

    Forgot to add the Logger.cxx...

---
+++ src/Logger.cxx
@@ -0,0 +1,19 @@
+#include <sstream>
+
+#include <libsxc/Logger.hxx>
+
+namespace libsxc
+{
+  Logger::Logger(const std::string file, unsigned int line)/*{{{*/
+  : _file(file)
+  , _line(line)
+  {
+  }/*}}}*/
+
+  const std::string Logger::_format(const std::string &msg)/*{{{*/
+  {
+    std::ostringstream s;
+    s << _file << ":" << _line << ": " << msg;
+    return s.str();
+  }/*}}}*/
+}
